88636ebf5da1aafc6a3dc7f67274a229dcb28a72
Fiona Runge <github@runjak.codes>
2025-06-22 10:37:21 +0200

    remove dead code

--- main.py
+++ main.py
@@ -55,81 +55,14 @@ def get_alphabet(prefix: str, suffix: str) -> Alphabet:
   letters = "".join(letters.split())
   return sorted(list(set(letters)))
 
-def alphabet_to_dict(alphabet: Alphabet) -> dict[str, int]:
-  return {k: v for (v, k) in enumerate(alphabet)}
-
-def vector_sum(*vectors: list[Vector]) -> Vector:
-  result = {}
-  for vector in vectors:
-    for k, v in vector.items():
-      result[k] = v + result.get(k, 0)
-  return result
-
 def vector_scale(vector: Vector, factor: int) -> Vector:
   return {k: v * factor for k, v in vector.items()}
-
-def vector_max(*vectors: list[Vector]) -> Vector:
-  result = {}
-  for vector in vectors:
-    for k, v in vector.items():
-      result[k] = max(v, result.get(k, 0))
-  return result
-
-def vector_eq(a:Vector, b:Vector) -> bool:
-  keys = set(a.keys()) or set(b.keys())
-  for k in keys:
-    if a.get(k, 0) != b.get(k, 0):
-      return False
-  
-  return True
-
-def manhattan_distance(a: Vector, b: Vector) -> int:
-  keys = set(a.keys()) + set(b.keys())
-  return sum([abs(a[k] - b[k]) for k in keys])
 
 def count_chars(s: str) -> Vector:
   counts = {}
   for c in "".join(s.split()):
     counts[c] = counts.get(c, 0) + 1
   return counts
-
-def get_lower_bounds(prefix: str) -> Vector:
-  return vector_scale(count_chars(spell_output({}, prefix)), 2)
-
-def get_alphabet_choices(alphabet: Alphabet, lower_bounds: Vector, upper_bound: int) -> dict[str, list[(int, pulp.LpVariable, Vector)]]:
-  alphabet_vectors = {}
-
-  for char in alphabet:
-    lower_bound = lower_bounds.get(char, 0)
-    alphabet_vectors[char] = [
-      (count, pulp.LpVariable(name=f"{char}_{count}", lowBound=0, upBound=1, cat=pulp.LpBinary),
-        # we scale by 2 because of the palindrome condition
-        vector_scale(count_chars(spell_char(char, count)), 2))
-      # we step by 2 because of the palindrome condition
-      for count in range(lower_bound, lower_bound + upper_bound, 2)]
-    
-  return alphabet_vectors
-
-def alphabet_choices_by_variable(alphabet_choices: dict[str, list[(int, pulp.LpVariable, Vector)]]) -> dict[pulp.LpVariable, (str, int)]:
-  return {
-    variable: (letter, count)
-    for letter, choices in alphabet_choices.items()
-    for (count, variable, _) in choices}
-
-def compute_contributors(alphabet_choices: dict[str, list[(int, pulp.LpVariable, Vector)]]) -> dict[str, list[(int, pulp.LpVariable)]]:
-  """
-  For every letter we compute:
-  - A list[(int, pulp.LpVariable)] of weights and variables that contribute to this letter.
-  """
-  contributors = {letter: [] for letter in alphabet_choices.keys()}
-
-  for letter_choices in alphabet_choices.values():
-    for (_, variable, vector) in letter_choices:
-      for letter, weight in vector.items():
-        contributors[letter] = contributors.get(letter, []) + [(weight, variable)]
-        # contributors[letter].append((weight, variable))
-  
-  return contributors
 
 def implies(a: pulp.LpVariable, b: pulp.LpVariable) -> pulp.LpConstraint:
   """
@@ -151,92 +84,6 @@ def implies(a: pulp.LpVariable, b: pulp.LpVariable) -> pulp.LpConstraint:
     a - b <= 0
   """
   return a - b <= 0
-
-def absolute(x: pulp.LpVariable):
-  """
-  How do you constrain the absolute value of some variable?
-
-  Structure that is {1, -1}: (1 - 2b)
-  """
-  None
-
-def letter_constraints(alphabet_choices: dict[str, list[(int, pulp.LpVariable, Vector)]], lower_bounds: Vector) -> list[pulp.LpConstraint]:
-  contributors = compute_contributors(alphabet_choices=alphabet_choices)
-  constraints = []
-
-  for letter, letter_choices in alphabet_choices.items():
-    contributor_sum = lower_bounds.get(letter, 0) + sum([weight * contributor_variable for (weight, contributor_variable) in contributors[letter]])
-    for (count, letter_variable, _) in letter_choices:
-      weighted_letter = count * letter_variable
-      constraints += [implies(weighted_letter, contributor_sum)]
-      # I'm not sure what to make of this. There's some kind of issue here and I don't know why/which.
-      # constraints += [implies(weighted_letter, contributor_sum), implies(contributor_sum, weighted_letter)]
-
-  return constraints
-
-def print_problem(problem: pulp.LpProblem, alphabet_choices: dict[str, list[(int, pulp.LpVariable, Vector)]]) -> None:
-  by_variable = alphabet_choices_by_variable(alphabet_choices=alphabet_choices)
-  vector = {}
-  
-  print(f"Problem status: {pulp.LpStatus[problem.status]}")
-  for v in problem.variables():
-    value = int(v.varValue)
-    if value != 0:
-      print(f"{v.name}={v.varValue}")
-      (letter, count) = by_variable[v]
-      vector[letter] = count
-  
-  print(spell_instructions(vector, default_prefix, default_suffix))
-
-def main():
-  prefix = default_prefix
-  suffix = default_suffix
-  lower_bounds = get_lower_bounds(prefix)
-  upper_bound = 100
-  alphabet = get_alphabet(prefix, suffix)
-
-  print(f"Alphabet: {alphabet}")
-  print(f"Lower bounds: {lower_bounds}")
-
-  problem = pulp.LpProblem(name="Fiddling", sense=pulp.LpMinimize)
-
-  alphabet_choices = get_alphabet_choices(
-    alphabet=alphabet,
-    lower_bounds=lower_bounds,
-    upper_bound=upper_bound)
-
-  """
-  What do we even optimize here?
-  We want to minimize a distance.
-  The distance is between:
-  - The sum of all picked per-letter weights
-  - The sum of all picked contributing weights plus the base vector
-  """
-  sum_of_per_letter_weights = sum([
-    weight * letter_variable
-    for letter_choices in alphabet_choices.values()
-    for (weight, letter_variable, _) in letter_choices
-    ])
-  
-  # FIXME is this right to just sum up, or should it be the sum of per-letter distances instead?
-  # It is not right, but maybe still interesting o.O
-
-  contributors = compute_contributors(alphabet_choices=alphabet_choices)
-  sum_of_all_contributing_weights_plus_base = None
-  for letter, letter_contributors in contributors.items():
-    base = lower_bounds.get(letter, 0)
-    None
-
-  # For every letter we choose at exactly one variable
-  for choices in alphabet_choices.values():
-    problem += sum([variable for (_, variable, _) in choices]) == 1
-  
-  for constraint in letter_constraints(alphabet_choices=alphabet_choices, lower_bounds=lower_bounds):
-    problem += constraint
-
-  problem.solve(solver=pulp.HiGHS())
-  print_problem(problem, alphabet_choices=alphabet_choices)
-
 
 def experiment_e():
   print("Experiment e\n")
@@ -281,7 +128,7 @@ def new_main():
   alphabet = get_alphabet(prefix=prefix, suffix=suffix)
   bound_delta = 100
 
-  lower_bounds = {letter: 0 for letter in alphabet} | get_lower_bounds(prefix)
+  lower_bounds = {letter: 0 for letter in alphabet} | vector_scale(count_chars(spell_output({}, prefix)), 2)
   upper_bounds = {letter: count + bound_delta for letter, count in lower_bounds.items()}
 
   letter_variables_counts = {letter: {pulp.LpVariable(name=f"{letter}_{count}", cat=pulp.LpBinary): count for count in range(lower_bounds[letter], upper_bounds[letter] + 1, 2)} for letter in alphabet}
@@ -321,15 +168,6 @@ def new_main():
     value = int(v.varValue)
     if value != 0:
       print(f"{v.name}={value} ({v.varValue})")
-
-def explore():
-  solution = {'*': 6,',': 6,':': 6,'W': 6,'a': 18,'b': 2,'c': 14,'d': 22,'g': 12,'h': 36,'i': 68,'l': 14,'m': 6,'n': 46,'p': 6,'q': 6,'s': 112,'t': 84,'u': 28,'v': 6,'w': 30,'x': 32,'y': 34,'❛': 62,'❜': 62}
-  print(f"Exploring solution:\n{solution!r}")
-  spelled = spell_output(solution)
-  print(f"Solution looks like this:\n{spelled!r}")
-  actual = count_chars(spelled)
-  difference = {k: v - actual.get(k, 0) for k, v in solution.items()}
-  print(f"difference is:\n{difference!r}")
 
 def thingy():
   print("thingy()")
@@ -394,6 +232,4 @@ if __name__ == '__main__':
   print(f"pulp got these solvers: {pulp.listSolvers(True)!r}")
   # experiment_e()
   # new_main()
-  # explore()
   thingy()
-  # main()
