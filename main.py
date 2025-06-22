63c2aa6da3436843acea85ad675741163646c886
Fiona Runge <github@runjak.codes>
2025-06-22 14:04:43 +0200

    compute absolute value

--- main.py
+++ main.py
@@ -326,7 +326,7 @@ def thingy():
         )
         problem += lower_bounds[letter] + offset_sum + weighted_variables == 0
 
-    problem.solve(solver=pulp.HiGHS(threads=6))
+    problem.solve(solver=pulp.HiGHS())
 
     print(f"Problem status: {pulp.LpStatus[problem.status]}")
     solution = dict(
@@ -348,8 +348,46 @@ def thingy():
     print(f"letter: actual, expected:\n\t{actual_expected}")
 
 
+def absolute_example():
+    """
+    We're interesting in minimizing an absolute value.
+    Let's try our hands at that.
+    x in [0, 10]
+    f(x) = -x + 10
+    g(x) = x
+    We want minimal |f(x) - g(y)|.
+    """
+    print("absolute_example()")
+    x = pulp.LpVariable(name="x", lowBound=0, upBound=10, cat=pulp.LpInteger)
+
+    f_x = -x + 10
+    g_x = x
+
+    abs_f_g = pulp.LpVariable(name="abs_f_g", lowBound=0, cat=pulp.LpInteger)
+
+    abs_constraint_1 = abs_f_g >= f_x - g_x
+    abs_constraint_2 = abs_f_g >= -(f_x - g_x)
+
+    problem = pulp.LpProblem(name="absolute", sense=pulp.LpMinimize)
+
+    # We minimize the absolute value:
+    problem += abs_f_g
+
+    # We add the constraints for the absolute value:
+    problem += abs_constraint_1
+    problem += abs_constraint_2
+
+    problem.solve(solver=pulp.HiGHS())
+
+    print(f"Problem status: {pulp.LpStatus[problem.status]}")
+    for v in problem.variables():
+        value = int(v.varValue)
+        print(f"{v.name} = {value}")
+
+
 if __name__ == "__main__":
     # print(f"pulp got these solvers: {pulp.listSolvers(True)!r}")
     # experiment_e()
     # new_main()
-    thingy()
+    # thingy()
+    absolute_example()
