1f24def001bfbecd667bd8db3d7eb570df7066d9
Fiona Runge <github@runjak.codes>
2025-07-02 00:03:52 +0200

    add experiment-manhattan-multiple-letters

--- experiment-manhattan-alphabet.py
+++ experiment-manhattan-alphabet.py
@@ -1,16 +1,17 @@
 import pulp
-from main import count_chars, spell_char, spell_chars, manhattan
+from main import get_alphabet, count_chars, spell_char, spell_chars, manhattan
 
-def experiment_multiple_letters():
+
+def experiment_alphabet():
     """
-    We attempt to reformulate experiment_e to count multiple distinct letters.
+    We attempt to generalize experiment_multiple_letters to a whole alphabet.
+    In scope of this experiment we neglect the correct count for ',',
+    but try to count the other letters correctly.
     """
-    letters = ["e", "f", "t", "h"]
-    prefix = (
-        f"The number of {", ".join(f"❛{l}❜s" for l in letters)} in this text is:\n\t"
-    )
-    lower_bounds = {k: v for k, v in count_chars(prefix).items() if k in letters}
-    delta = 10
+    prefix = f"This text contains the following letters:\n"
+    letters = get_alphabet(prefix=prefix)
+    lower_bounds = {l: 0 for l in letters} | count_chars(prefix + "and")
+    delta = 50
     upper_bounds = {k: v + delta for k, v in lower_bounds.items()}
 
     variables: dict[str, dict[pulp.LpVariable, int]] = {
@@ -32,7 +33,7 @@ def experiment_multiple_letters():
                 if offset_letter in offsets:
                     offsets[offset_letter].append((offset_count, variable))
 
-    problem = pulp.LpProblem(name="Experiment_multiple_letters", sense=pulp.LpMinimize)
+    problem = pulp.LpProblem(name="Experiment_alphabet", sense=pulp.LpMinimize)
 
     manhattan_pairs = []
     for letter, choices in variables.items():
@@ -41,6 +42,7 @@ def experiment_multiple_letters():
         )
 
         offset_sum = sum([weight * variable for weight, variable in offsets[letter]])
+        # FIXME count commas
         manhattan_pairs.append((lower_bounds[letter] + offset_sum, weighted_choice))
 
     manhattan_goal, manhattan_constraints = manhattan(manhattan_pairs)
@@ -61,12 +63,36 @@ def experiment_multiple_letters():
         if value != 0:
             print(f"{v.name}={v.varValue}")
 
-    counts: dict[str, int] = {}
+    expected_counts: dict[str, int] = {}
     for letter, choices in variables.items():
         for variable, count in choices.items():
             if int(variable.varValue) > 0:
-                counts[letter] = count
+                expected_counts[letter] = count
 
-    print(f"{prefix}{spell_chars(counts)}")
+    output = f"{prefix}{spell_chars(expected_counts)}"
+    print(output)
 
-experiment_multiple_letters()
+    actual_counts = count_chars(output)
+    letters = set(expected_counts.keys()) and set(actual_counts.keys())
+    count_differences = {
+        letter: expected_counts.get(letter, 0) - actual_counts.get(letter, 0)
+        for letter in letters
+    }
+
+    print(f"Count differences (expected - actual):\n{count_differences}")
+
+
+experiment_alphabet()
+
+"""
+Produces this output:
+---
+This text contains the following letters:
+six ❛-❜s, two ❛:❜s, two ❛T❜s, three ❛a❜s, two ❛c❜s, two ❛d❜s, thirty-one ❛e❜s,
+seven ❛f❜s, five ❛g❜s, nine ❛h❜s, eighteen ❛i❜s, five ❛l❜s, seventeen ❛n❜s,
+eleven ❛o❜s, six ❛r❜s, thirty-four ❛s❜s, twenty-eight ❛t❜s, two ❛u❜s, eight ❛v❜s,
+ten ❛w❜s, six ❛x❜s, six ❛y❜s, twenty-five ❛❛❜s and twenty-five ❛❜❜s
+Count differences (expected - actual):
+{'❛': 0, 'y': 0, 'o': 0, 's': 0, ':': 0, 'n': 0, 'd': 0, 't': 0, 'a': 0, 'l': 0, 'g': 0, 'x': 0,
+ ',': -22, 'c': 0, 'v': 0, 'h': 0, '-': 0, 'T': 0, '❜': 0, 'i': 0, 'u': 0, 'w': 0, 'e': 0, 'r': 0, 'f': 0}
+"""
